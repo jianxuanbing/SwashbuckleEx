5aa3a14f82c67229512e74a4f4b2ffe7af33b7c3
jian玄冰 <jianxuanhuo1@126.com>
2017-08-16 20:54:57 +0800

    更新库

--- Swashbuckle.WebHost/Properties/AssemblyInfo.cs
+++ Swashbuckle.WebHost/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.6")]
 [assembly: AssemblyFileVersion("1.0.0.6")]
-[assembly:AssemblyInformationalVersion("1.0.06")]
+[assembly: AssemblyInformationalVersion("1.0.0.6")]
